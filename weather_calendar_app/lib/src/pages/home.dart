e0339482834b413cec18af48d3501f64529e2e2f
Arthur Denner <arthurdenner7@gmail.com>
2020-04-05 00:23:25 +0200

    feat: style header and add avatar

--- weather_calendar_app/lib/src/pages/home.dart
+++ weather_calendar_app/lib/src/pages/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-const SIDEBAR_SIZE = 0.3;
+const double SIDEBAR_SIZE = 0.3;
+const double AVATAR_SIZE = 30;
+const AVATAR =
+    'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -11,35 +14,66 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: Column(
           children: <Widget>[
-            _buildHeader(),
+            _buildHeader(context),
+            SizedBox(height: 50),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildHeader() {
+  Widget _buildHeader(BuildContext context) {
+    final sidebarSize = MediaQuery.of(context).size.width * SIDEBAR_SIZE;
+
     return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 50,
-        vertical: 20,
-      ),
+      padding: EdgeInsets.fromLTRB(sidebarSize, 20, 50, 50),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text('Los Angeles'),
-              Text('Feb 03, Tuesday'),
+              Text(
+                'Los Angeles',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Feb 03, Tuesday',
+                style: TextStyle(
+                  color: Colors.black26,
+                ),
+              ),
             ],
           ),
           Row(
             children: <Widget>[
-              Icon(Icons.person),
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: CircleAvatar(
+                  radius: AVATAR_SIZE,
+                  child: Image.network(AVATAR),
+                ),
+              ),
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text('Alexander Paul'),
-                  Text('Basic level'),
+                  Text(
+                    'Alexander Paul',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    'Basic level',
+                    style: TextStyle(
+                      color: Colors.black26,
+                    ),
+                  ),
                 ],
               ),
             ],
