c4b6fbb6aef337cfc9be4481d0f34449ee0783bd
Arthur Denner <arthurdenner7@gmail.com>
2020-04-12 01:56:19 +0200

    feat: add blur and selected size state

--- nike_app/lib/pages/product_page/product_page.dart
+++ nike_app/lib/pages/product_page/product_page.dart
@@ -1,8 +1,10 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:nike_app/pages/product_page/widgets/app_bar.dart';
 import 'package:nike_app/pages/product_page/widgets/images.dart';
-import 'package:nike_app/pages/product_page/widgets/sidebar.dart';
 import 'package:nike_app/utils/constants.dart';
+
+const sizes = ['S', 'M', 'L', 'XL', '2XL', '3XL'];
 
 class ProductPage extends StatefulWidget {
   ProductPage({Key key}) : super(key: key);
@@ -12,6 +14,8 @@ class ProductPage extends StatefulWidget {
 }
 
 class _ProductPageState extends State<ProductPage> {
+  String selectedSize = 'M';
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -31,72 +35,85 @@ class _ProductPageState extends State<ProductPage> {
                     child: Container(
                       height: MediaQuery.of(context).size.height * 0.38,
                       margin: EdgeInsets.all(8.0),
-                      padding: EdgeInsets.all(16.0),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(30),
-                        gradient: LinearGradient(
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          colors: [
-                            Colors.white,
-                            Colors.white.withOpacity(0.7),
-                          ],
-                        ),
                       ),
-                      child: Column(
-                        children: <Widget>[
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              _buildSize('S'),
-                              _buildSize('M'),
-                              _buildSize('L'),
-                              _buildSize('XL'),
-                              _buildSize('2XL'),
-                              _buildSize('3XL'),
-                            ],
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.only(top: 20, bottom: 30),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: <Widget>[
-                                  Text(
-                                    "Men's Running Jacket".toUpperCase(),
-                                    style: TextStyle(
-                                      color: Color(0xFF868686),
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.symmetric(
-                                      horizontal: 50,
-                                    ),
-                                    child: Text(
-                                      "Nike Windrunner Wild Run".toUpperCase(),
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 24,
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    "\$120",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
-                                  ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              // color: Colors.grey.shade200.withOpacity(0.5),
+                              gradient: LinearGradient(
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                                stops: [0.5, 0.85, 0.9],
+                                colors: [
+                                  Colors.white,
+                                  Colors.white.withOpacity(.9),
+                                  Colors.white.withOpacity(.8)
                                 ],
                               ),
                             ),
+                            child: Column(
+                              children: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(16),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: sizes.map(_buildSize).toList(),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Container(
+                                    padding: EdgeInsets.only(
+                                      top: 20,
+                                      bottom: 30,
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          "Men's Running Jacket".toUpperCase(),
+                                          style: TextStyle(
+                                            color: Color(0xFF868686),
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Padding(
+                                          padding: EdgeInsets.symmetric(
+                                            horizontal: 50,
+                                          ),
+                                          child: Text(
+                                            "Nike Windrunner Wild Run"
+                                                .toUpperCase(),
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 24,
+                                            ),
+                                          ),
+                                        ),
+                                        Text(
+                                          "\$120",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
-                        ],
+                        ),
                       ),
                     ),
                   ),
@@ -110,9 +127,11 @@ class _ProductPageState extends State<ProductPage> {
   }
 
   Widget _buildSize(String label) {
+    final isSelected = label == selectedSize;
+
     return Expanded(
       child: Container(
-        color: Colors.white,
+        color: isSelected ? Colors.white : null,
         margin: EdgeInsets.symmetric(horizontal: 6),
         padding: EdgeInsets.symmetric(vertical: 16),
         child: Center(
