811f230321e7b947f8fbc7e44933a2a22a63e3ca
Arthur Denner <arthurdenner7@gmail.com>
2020-04-12 02:38:32 +0200

    refactor: move add to cart to its own widget

--- nike_app/lib/pages/product_page/product_page.dart
+++ nike_app/lib/pages/product_page/product_page.dart
@@ -1,10 +1,8 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:nike_app/pages/product_page/widgets/add_to_cart.dart';
 import 'package:nike_app/pages/product_page/widgets/app_bar.dart';
 import 'package:nike_app/pages/product_page/widgets/card.dart';
 import 'package:nike_app/pages/product_page/widgets/images.dart';
-import 'package:nike_app/pages/product_page/widgets/info.dart';
-import 'package:nike_app/pages/product_page/widgets/sizes.dart';
 import 'package:nike_app/utils/constants.dart';
 
 class ProductPage extends StatefulWidget {
@@ -28,43 +26,7 @@ class _ProductPageState extends State<ProductPage> {
                 children: <Widget>[
                   ProductImages(),
                   ProductCart(),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      height: 70,
-                      margin: EdgeInsets.symmetric(horizontal: 30),
-                      padding: EdgeInsets.symmetric(horizontal: 30),
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(35),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Icon(
-                            Icons.shopping_cart,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            'Add to Cart'.toUpperCase(),
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            '\$120',
-                            style: TextStyle(
-                              color: Color(0xFFC7C7C7),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
+                  AddToCartButton(),
                 ],
               ),
             ),
---
+++ nike_app/lib/pages/product_page/widgets/add_to_cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class AddToCartButton extends StatelessWidget {
+  const AddToCartButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Container(
+        height: 70,
+        margin: EdgeInsets.symmetric(horizontal: 30),
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(35),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
+            Text(
+              'Add to Cart'.toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '\$120',
+              style: TextStyle(
+                color: Color(0xFFC7C7C7),
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
