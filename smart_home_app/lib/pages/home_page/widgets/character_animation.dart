76fa4a6e509a0ce78ad972f3857ba84120cd5c32
Arthur Denner <arthurdenner7@gmail.com>
2020-04-20 14:12:29 +0200

    feat: add lightnings to character animation

--- smart_home_app/lib/pages/home_page/widgets/character_animation.dart
+++ smart_home_app/lib/pages/home_page/widgets/character_animation.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:smart_home_app/utils/constants.dart';
 
@@ -19,14 +20,26 @@ class _CharacterAnimationState extends State<CharacterAnimation> {
     final isToggled = widget.isToggled;
     final _height = MediaQuery.of(context).size.height;
     final _screenWidth = MediaQuery.of(context).size.width;
+
     final _cloud1Top = isToggled ? _height * -.1 : _height * .03;
     final _cloud2Top = isToggled ? _height * -.1 : _height * .05;
+
     final _bubbleLeft = isToggled ? _screenWidth / 2 : 0.0;
     final _bubbleRight = isToggled ? _screenWidth * -1 : 0.0;
     final _bubbleRadius = isToggled ? _screenWidth * 1.5 : _screenWidth / 2;
     final _bubbleTop = isToggled ? -(_bubbleRadius / 4) : -(_bubbleRadius / 2);
+
     final _dollTop = isToggled ? _height * .05 : _height / 4;
     final _dollHeight = isToggled ? _height * .27 : 0.0;
+
+    final _lightning1Top = isToggled ? _height * .05 : _height / 4;
+    final _lightning1Left = isToggled ? _screenWidth * .25 : _screenWidth * .5;
+    final _lightning2Top = isToggled ? _height * .12 : _height / 4;
+    final _lightning2Left = isToggled ? _screenWidth * .1 : _screenWidth * .5;
+    final _lightning3Top = isToggled ? _height * .04 : _height / 4;
+    final _lightning3Left = isToggled ? _screenWidth * .65 : _screenWidth * .5;
+    final _lightning4Top = isToggled ? _height * .15 : _height / 4;
+    final _lightning4Left = isToggled ? _screenWidth * .8 : _screenWidth * .5;
 
     return Container(
       height: isToggled ? _height * .45 : _height * .4,
@@ -58,6 +71,50 @@ class _CharacterAnimationState extends State<CharacterAnimation> {
               child: Image.network(
                 AppImages.boneco,
                 fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: AppDurations.short,
+            top: _lightning1Top,
+            left: _lightning1Left,
+            child: Image.network(
+              AppImages.raio,
+              width: 25,
+            ),
+          ),
+          AnimatedPositioned(
+            duration: AppDurations.short,
+            top: _lightning2Top,
+            left: _lightning2Left,
+            child: Image.network(
+              AppImages.raio,
+              width: 35,
+            ),
+          ),
+          AnimatedPositioned(
+            duration: AppDurations.short,
+            top: _lightning3Top,
+            left: _lightning3Left,
+            child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(pi),
+              child: Image.network(
+                AppImages.raio,
+                width: 30,
+              ),
+            ),
+          ),
+          AnimatedPositioned(
+            duration: AppDurations.short,
+            top: _lightning4Top,
+            left: _lightning4Left,
+            child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(pi),
+              child: Image.network(
+                AppImages.raio,
+                width: 35,
               ),
             ),
           ),
