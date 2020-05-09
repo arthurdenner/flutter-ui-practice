2b628a8b24cae8f5b467cb50d3accb758e96d1fd
Arthur Denner <arthurdenner7@gmail.com>
2020-05-09 23:40:29 +0200

    chore: initial commit

---
+++ email_client/android/app/src/main/kotlin/com/example/email_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.email_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ email_client/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:email_client/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      title: 'Email Client',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
